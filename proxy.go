4e5130dd019edac8525943c3c1da1fa36718defc
wangbinru <wangbinru@sogou-inc.com>
2020-11-10 12:28:19 +0800

    update

--- proxy.go
+++ proxy.go
@@ -641,7 +641,7 @@ func (p *Proxy) forwardTunnel(ctx *Context, rw http.ResponseWriter) {
 			Method: "CONNECT",
 			URL:    &url.URL{Opaque: ctx.Req.URL.Host},
 			Host:   ctx.Req.URL.Host,
-			Header: make(http.Header),
+			Header: CloneHeader(ctx.Req.Header),
 		}
 		u := parentProxyURL.User
 		if u != nil {
